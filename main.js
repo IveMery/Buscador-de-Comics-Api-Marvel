d01490c7ee28e93a248e99fa8f47a3678b2255a6
IvetteMery <ivette.mery@gmail.com>
2021-02-13 20:26:11 -0300

    agregue funcion para seleccionar comics

--- main.js
+++ main.js
@@ -22,32 +22,47 @@ const buscarComics = (url, nombre) => {
             const resultados = document.querySelector('.resultados')
             resultados.innerHTML = ""
             comics.data.results.map(comic => {
-                    resultados.innerHTML += `<article data-id="${comic.id}" class="comic">
+                resultados.innerHTML += `<article data-id="${comic.id}" class="comic">
 <div class="comic-img-container">
    <img src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="" class="comic-img-portada">
 </div>
 <h3 class="comic-titulo"> ${comic[nombre]}</h3>
 </article>
 `
-                })
-                //     const listaDecomics = document.querySelectorAll('.comic');
-                //     console.log(listaDecomics)
-                //     listaDecomics.forEach(comic => {
-                //         comic.onclick = () => {
-                //             console.log("hice click")
-                //             console.log(comic.dataset.id)
-                //             fetch(`https://gateway.marvel.com/v1/public/comics/${comic.dataset.id}?${ apiKey}`)
-                //             .then(res=> res.json())
-                //             .then(dataComic =>{
-                //                 
+            })
+            const listaDecomics = document.querySelectorAll('.comic');
+            console.log(listaDecomics)
+            listaDecomics.forEach(comic => {
+                comic.onclick = () => {
 
-            //             })
-            //         }
-            //     })
-            // })
+                    console.log("hice click a un comic con el id ", comic.dataset.id)
 
+                    fetch(`https://gateway.marvel.com/v1/public/comics/${comic.dataset.id}?apikey=fa4aa6e4fe839711358c5ab717c2ea05`)
 
-
+                    .then(res => res.json())
+                        .then(dataComic => {
+                            console.log(dataComic)
+                            resultados.innerHTML = ""
+                            dataComic.data.results.map(datosComic => {
+                                resultados.innerHTML = `
+<article class="info-comic">
+                            <div class="info-comic-img">
+                            <img src="${datosComic.thumbnail.path}.${datosComic.thumbnail.extension}" alt="" class="info-comic-img-portada">
+                            </div>
+                            <div class="info-comic-datos">
+<h3 class="comic-titulo"> ${datosComic.title}</h3>
+                            <p>Publicado:</p>
+                            <p>${datosComic.dates[0].date}</p>
+                            <p> Guionistas:</p>
+                            <p>${datosComic.creators.items[0].name}</p>
+                            <p> Descripción:</p>
+                            <p>${datosComic.description}</p>
+                            </div>
+                            </article>`
+                            })
+                        })
+                }
+            })
         })
 }
 
